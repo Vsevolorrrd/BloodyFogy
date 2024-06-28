533b9d93bc2e20e3d5ad1ca70b9b943c97f4c41a
Vsevolord Kolomytsev <vsevolod.kolomytsev@student.saedubai.comProfile image>
2024-06-29 03:04:40 +0400

    Stoping suicides
    Added a layer of enemies for axes to stop them killing themselves.

--- Assets/Prefabs/NPC/Weapons/Melee/Axe.cs
+++ Assets/Prefabs/NPC/Weapons/Melee/Axe.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Axe : MonoBehaviour
 {
     public float damage = 40f;
+    public LayerMask Enemy;
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.layer == Enemy)
         {
-            collision.GetComponent<PlayerHealth>().PlayerTakeDamage(damage);
-        }
-        if (collision.gameObject.tag == "AI")
-        {
-            collision.GetComponent<EnemyHealth>().DamageAi(damage);
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.GetComponent<PlayerHealth>().PlayerTakeDamage(damage);
+            }
+            if (collision.gameObject.tag == "AI")
+            {
+                collision.GetComponent<EnemyHealth>().DamageAi(damage);
+            }
         }
     }
 }
